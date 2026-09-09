1f10c146a5204f8e310c73e771f679e41fdc896c
scott90030 <100068786+scott90030@users.noreply.github.com>
2026-09-09 10:02:20 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,5 @@
-const CACHE='travel-checklist-pwa-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-180.png','./icon-512.png'];
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-  );
-  self.clients.claim();
-});
-self.addEventListener('fetch',event=>{
-  event.respondWith(
-    caches.match(event.request).then(cached=>cached||fetch(event.request))
-  );
-});
+const CACHE='travel-checklist-v3';
+const A=['./','./index.html','./manifest.json','./icon-180.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
